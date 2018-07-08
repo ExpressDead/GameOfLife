911274dd537ce42b21ec4b6f8017b83af7974087
Doyle Turner <dturner@meditract.com>
2018-07-07 20:38:55 -0400

    adding correct files back to repo

--- Existence.Test/RulesOfLifeTests.cs
+++ Existence.Test/RulesOfLifeTests.cs
@@ -2,12 +2,36 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Existence.Test
 {
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+	[TestClass]
+	public class RulesOfLifeTests
+	{
+		[TestMethod] //Rule 1 "Under Population"
+		public void When_LiveCellHasLessThan2LiveNeighbors_TheCellDies()
+		{
+			         
+	    }
+        [TestMethod] //Rule 2 
+        public void When_LiveCellHas2LiveNeighbors_TheCellLives()
+		{
+			
+		}
+
+        [TestMethod] //Rule 2
+		public void When_LiveCellHas3LiveNeighbors_TheCellLives()
+		{
+			
+		}
+
+        [TestMethod] //Rule 3 "Overpopulation"
+        public void When_LiveCellHasMoreThan3Neighbors_TheCellDies()
+		{
+			
+		}
+
+        [TestMethod] //Rule 4 "Reproduction"
+        public void Whne_DeadCellHasExactly3Neighbors_TheCellLives()
+		{
+			
+		}
     }
 }
--- Existence/Game.cs
+++ Existence/Game.cs
@@ -2,7 +2,19 @@
 
 namespace Existence
 {
-    public class Class1
+    public class Game
     {
+		//Basic components for the game. Defines the size of the world and holds the current generation.
+		public int Columns { get; } 
+        public int Rows { get; }
+		public int[,] Grid { get; }
+
+		public Game(int cols, int rows, int[,] grid)
+		{
+			Columns = cols;
+			Rows = rows;
+			Grid = grid;
+		}
+
     }
 }
